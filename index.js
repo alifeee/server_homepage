c89927fbefe14fa8160ce28bd98dbf704b667214
alifeee <alifeeerenn@hotmail.com>
2023-03-12 20:29:59 +0000

    reformat code

--- index.js
+++ index.js
@@ -22,15 +22,49 @@ document.addEventListener("keydown", function (e) {
 
 function parseCommand(text) {
   if (text.includes("help")) {
-    return "<span class='result'>Commands: help, ls, clear</span>";
+    return `
+    <span class='result'>
+      Commands: help, ls, clear
+    </span>`;
   } else if (
     text.startsWith("ls") ||
     text.startsWith("dir") ||
     text.startsWith("list-projects")
   ) {
-    return "<table class='result'><tr><th>Name</th><th>Docker image</th><th>GitHub</th></tr><tr><td>---</td><td>---</td><td>---</td></tr><tr><td>Steam collage</td><td>☑</td><td><a href='https://github.com/alifeee/steam_collage_api'>https://github.com/alifeee/steam_collage_api</a></td></tr></table>";
+    return `
+    <table class='result'>
+      <tr>
+        <th>Name</th>
+        <th>Docker image</th>
+        <th>Port</th>
+        <th>GitHub</th>
+      </tr>
+      <tr>
+        <td>---</td><td>---</td><td>---</td><td>---</td>
+      </tr>
+      <tr>
+        <td>Homepage</td>
+        <td>☑</td>
+        <td>80</td>
+        <td>
+          <a href='https://github.com/alifeee/server_homepage'>https://github.com/alifeee/server_homepage</a>
+        </td>
+      </tr>
+      <tr>
+        <td>Steam collage API</td>
+        <td>☑</td>
+        <td>5000</td>
+        <td>
+          <a href='https://github.com/alifeee/steam_collage_api'>https://github.com/alifeee/steam_collage_api</a>
+        </td>
+      </tr>
+    </table>`;
   } else if (text.startsWith("cd")) {
-    return "<span class='result'>No</span>";
+    return `
+    <span class='result'>
+      No
+    </span>
+    `;
   } else if (text.startsWith("clear")) {
     document.querySelectorAll(".console > *").forEach((el) => {
       if (el.id !== "bottom") {
@@ -39,3 +73,14 @@ function parseCommand(text) {
     });
   }
 }
+
+document.querySelector("#bottom").insertAdjacentHTML(
+  "beforebegin",
+  `
+    <span class=command>
+      list-projects
+    </span>
+    <br/>
+    ${parseCommand("ls")}
+    `
+);
